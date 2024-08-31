7eb01ba0a42716b189529b6229a54507fb8d34b6
Qusai GAZAWY <92310650+cando-jo@users.noreply.github.com>
2024-08-31 12:31:48 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,119 @@
+#pgzero
+import random
+
+WIDTH = 600
+HEIGHT = 450
+
+TITLE = "Uzay Yolculuğu"
+FPS = 30
+
+# Değişkanler ve Nesneler
+gemi = Actor("gemi", (300, 400))
+uzay = Actor("uzay")
+
+#gemiler 150 300 450
+gemi1 = Actor('gemi1', (150, 225))
+gemi2 = Actor('gemi2', (300, 225))
+gemi3 = Actor('gemi3', (450, 225))
+
+
+dusmanlar = []
+gezegenler = [Actor("gezegen1", (random.randint(0, 600), -100)), Actor("gezegen2", (random.randint(0, 600), -100)), Actor("gezegen3", (random.randint(0, 600), -100))]
+meteorlar = []
+mod = 'menu'
+
+# Düşmanlar listesini oluşturmak
+for i in range(5):
+    x = random.randint(0, 600)
+    y = random.randint(-450, -50)
+    dusman = Actor("düşman", (x, y))
+    dusman.speed = random.randint(2, 8)
+    dusmanlar.append(dusman)
+    
+# Meteorlar listesini oluşturmak
+for i in range(5):
+    x = random.randint(0, 600)
+    y = random.randint(-450, -50)
+    meteor = Actor("meteor", (x, y))
+    meteor.speed = random.randint(2, 10)
+    meteorlar.append(meteor)
+
+# Çizimler
+def draw():
+    # Oyun Modu
+    if mod == 'oyun':
+        uzay.draw()
+        gezegenler[0].draw()
+        # Meteorların Çizilmesi
+        for i in range(len(meteorlar)):
+            meteorlar[i].draw()
+        gemi.draw()
+        # Düşmanların Çizilmesi
+        for i in range(len(dusmanlar)):
+            dusmanlar[i].draw()
+    # Oyun Bitti ekranı
+    elif mod == 'son':
+        uzay.draw()
+        screen.draw.text("OYUN BİTTİ!", center = (300, 200), color = "white", fontsize = 36)
+        
+    elif mod == 'menu':
+        uzay.draw()
+        gemi1.draw()
+        gemi2.draw()
+        gemi3.draw()
+        screen.draw.text("Choose your spaceship!!", center = (300, 100), color = "white", fontsize = 36)
+    
+# Kontroller
+def on_mouse_move(pos):
+    gemi.pos = pos
+
+# Yeni Düşmanların Eklenmesi
+def yeni_dusman():
+    x = random.randint(0, 400)
+    y = -50
+    dusman = Actor("düşman", (x, y))
+    dusman.speed = random.randint(2, 8)
+    dusmanlar.append(dusman)
+
+# Düşmanların Hareketleri
+def dusman_gemisi():
+    for i in range(len(dusmanlar)):
+        if dusmanlar[i].y < 650:
+            dusmanlar[i].y = dusmanlar[i].y + dusmanlar[i].speed
+        else:
+            dusmanlar.pop(i)
+            yeni_dusman()
+
+# Gezegenlerin Hareketleri
+def gezegen():
+    if gezegenler[0].y < 550:
+            gezegenler[0].y = gezegenler[0].y + 1
+    else:
+        gezegenler[0].y = -100
+        gezegenler[0].x = random.randint(0, 600)
+        birinci = gezegenler.pop(0)
+        gezegenler.append(birinci)
+
+# Meteorların Hareketleri
+def meteorlar_hareket():
+    for i in range(len(meteorlar)):
+        if meteorlar[i].y < 450:
+            meteorlar[i].y = meteorlar[i].y + meteorlar[i].speed
+        else:
+            meteorlar[i].x = random.randint(0, 600)
+            meteorlar[i].y = -20
+            meteorlar[i].speed = random.randint(2, 10)
+
+# Çarpışmalar
+def carpismalar():
+    global mod
+    for i in range(len(dusmanlar)):
+        if gemi.colliderect(dusmanlar[i]):
+            mod = 'son'
+
+def update(dt):
+    if mod == 'oyun':
+        dusman_gemisi()
+        carpismalar()
+        gezegen()
+        meteorlar_hareket()
